3cd259a656c9a917080130d1c59a4efa963edf41
syairsgphksdy <122209244+syairsgphksdy@users.noreply.github.com>
2023-01-09 21:30:52 +0800

    header.js

--- header.js
+++ header.js
@@ -4,7 +4,7 @@ _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
 var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//cdn.statically.io/gh/syairsgphksdy/togel/main/js15_as.js');
+hs.src = ('//rawcdn.githack.com/syairsgphksdy/togel/2e19ff423f5f561dd39acc67b4ff17724634ef56/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();
 
